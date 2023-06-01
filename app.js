cf88bcd142801e43abafa23b3a53fa01e385466f
Szade <wojciech.szade@gmail.com>
2023-06-01 12:29:38 +0200

    added IP and port selection for hosting the app

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ app.use('/ipv4', require('./routes/ipv4'));
 app.use('/ipv6', require('./routes/ipv6'));
 app.use('/dns', require('./routes/dns'));
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const networkInterface = process.env.NETWORK_INTERFACE || 'eth0'; 
+const networkIP = process.env.NETWORK_IP || 'localhost';
+const port = process.env.NETWORK_PORT || 3000;
+
+app.listen(port, networkIP, () => {
+  console.log(`Server running on network interface ${networkInterface} on IP ${networkIP} on port ${port}`);
 });
